39b9875f730345289bd3b4190f5108953149f40f
Lucas Gelinas <lucas.gelinas@ocsbstudent.ca>
2021-02-18 15:30:53 +0000

    Might be final edit

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-//connect "Guess" button to "random" function 
+// connect "Guess" button to "random" function
 document.getElementById('button').addEventListener('click', randomizer)
 let randomNumber = 0
 let userGuess = 0
 
-function randomizer() {
-  //get number typed in by user and covert to an integer 
+function randomizer () {
+  // get number typed in by user and covert to an integer
   userGuess = document.getElementById('input').value
   userGuess = parseInt(userGuess)
 
-  //generate random number 
+  // generate random number
   randomNumber = Math.random() * 6 + 1
   randomNumber = parseInt(randomNumber)
   
-  //compare the numbers 
-  if (userGuess == randomNumber) {
-    document.getElementById('answer').innerHTML = "You got it right!"
+  // compare the numbers
+  if (userGuess === randomNumber) {
+    document.getElementById('answer').innerHTML = 'You got it right!'
   }
-  
 }
